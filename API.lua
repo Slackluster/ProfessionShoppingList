37b3bb9c6305b3e4ef92130d84b9f423dd5d48d4
Slackluster <ruben.sluimer@gmail.com>
2025-08-09 21:25:15 +0200

    Added API documentation

---
+++ API.lua
@@ -0,0 +1,12 @@
+---------------------------------------
+-- Profession Shopping List: API.lua --
+---------------------------------------
+
+-- Switch between Ragnaros and Pierre for the button PSL adds
+ProfessionShoppingList.SwapCookingPet()
+
+-- Check if an item's appearance is collected; returns true or false
+ProfessionShoppingList.IsAppearanceCollected(itemLink)
+
+-- Check if an item's source is collected; returns true or false
+ProfessionShoppingList.IsSourceCollected(itemLink)
