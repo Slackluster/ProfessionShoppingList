481f90e1db14a150e471c3b3dbbdb80b8b7c3d23
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-05-19 17:36:28 +0200

    Load our patron enhancements (if enabled) even with NMNM enabled. Make the users pick one!

--- modules-old/CraftingOrders.lua
+++ modules-old/CraftingOrders.lua
@@ -574,7 +574,7 @@ app.Event:Register("CRAFTINGORDERS_UPDATE_ORDER_COUNT", function(orderType, numO
 		app.OrderInfo = app.OrderInfo or {}
 
 		local function OnFrameInitialized(_, v, data)
-			if v and data and v.cells and not C_AddOns.IsAddOnLoaded("PublicOrdersReagentsColumn") then
+			if v and data and v.cells then
 				if not data.option or not data.option.orderID then return end
 				app.OrderAdjustments[v] = app.OrderAdjustments[v] or {}
 
