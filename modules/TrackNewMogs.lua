d14d87f1b08c96d29bdc928732afd16406c114a2
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-08-23 23:05:01 +0200

    Don't need to define a false flag

--- modules/TrackNewMogs.lua
+++ modules/TrackNewMogs.lua
@@ -13,7 +13,6 @@ local L = app.locales
 app.Event:Register("ADDON_LOADED", function(addOnName, containsBindings)
 	if addOnName == appName then
 		app.Tooltip = {}
-		app.Flag.ChangingRecipes = false
 	end
 end)
 
