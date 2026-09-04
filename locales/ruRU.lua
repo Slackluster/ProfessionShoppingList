6c53bb68702f7996cbcc4e817ab1b89ac6470a44
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-09-05 01:56:48 +0200

    Add a new slash command that lets you track all recipes using the linked reagent

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -225,6 +225,8 @@ L.SETTINGS_SLASH_TRACKACHIE =            "Отслеживайте рецепт�
 L.SETTINGS_SLASH_CRAFTINGACHIE =         "достижение профессий"
 L.SETTINGS_SLASH_RECIPEID =              "recipeID"
 L.SETTINGS_SLASH_QUANTITY =              "число"
+-- L.SETTINGS_SLASH_REAGENT =               "itemLink or itemID"
+-- L.SETTINGS_SLASH_TRACKREAGENT =          "Track all recipes using this reagent"
 
 L.GENERAL =                              GENERAL -- "General"
 L.SETTINGS_MINIMAP_TITLE =               "Показать значок на миникарте"
